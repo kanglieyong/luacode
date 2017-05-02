c970f7bdd56fcfabec5681598475c6d993cc0116
kly <kanglieyong@gmail.com>
2017-05-02 14:22:38 +0800

    update: function invoke

--- chapter05/functioninvoke.lua
+++ chapter05/functioninvoke.lua
@@ -7,3 +7,19 @@ print(x,y)
 
 x,y = foo0(), 20, 30
 print(x,y)
+
+print(foo2(), 1) --> a 1
+print(foo2().."x") --> ax
+
+
+function foo(i)
+  if i==0 then return foo0()
+  elseif i==1 then return foo1()
+  elseif i==2 then return foo2()
+  end
+end
+
+print(foo(1)) --> a
+print(foo(2)) -->a b
+print(foo(0)) --> no value
+print(foo(3)) --> no value
