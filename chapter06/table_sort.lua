7e0e68e32cd2cfd4c93a4f598825089ef846744b
kly <kanglieyong@gmail.com>
2017-05-02 17:47:22 +0800

    table:sort with order function

---
+++ chapter06/table_sort.lua
@@ -0,0 +1,8 @@
+network = {
+  {name = "grauna", IP="210.26.30.34"},
+  {name = "arraial", IP="210.26.30.23"},
+  {name = "lua", IP="210.26.30.12"},
+  {name = "derain", IP="210.26.30.20"},
+}
+
+table.sort(network, function(a,b) return (a.name > b.name) end)
